457b84c7ba77ee67ae5567dd576cfafbcb970cf1
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2023-12-09 20:05:31 +0300

    Import: alert component

--- client/src/components/ui/index.js
+++ client/src/components/ui/index.js
@@ -1,6 +1,7 @@
 export { default as Input } from './Input';
 export { default as Button } from './Button';
 export { default as ConfigProvider } from './ConfigProvider';
-export { default as FormItem } from  './Form/FormItem';
-export { default as FormContainer } from  './Form/FormContainer';
-export {default as Spinner} from "./Spinner";
+export { default as FormItem } from './Form/FormItem';
+export { default as FormContainer } from './Form/FormContainer';
+export { default as Spinner } from './Spinner';
+export { default as Alert } from './Alert';
