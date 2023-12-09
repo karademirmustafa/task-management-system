71076c2ad2ba64909533d661828407c78a7539e7
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2023-12-09 04:09:31 +0300

    Add : global error log

--- server/src/middlewares/error.js
+++ server/src/middlewares/error.js
@@ -1,7 +1,57 @@
 const {ErrorResponse} = require("../utils/error");
 
 const errorHandler = (err, req, res, next) => {
+  let storageData = {};
+  const startTime = new Date(); // Start time for measuring response time
 
+  const originalSend = res.send;
+  res.send = function (data) {
+    storageData.response = data;
+    originalSend.call(this, data);
+  };
+  res.on('finish', () => {
+    const statusCode = res.statusCode;
+    const contentType = res.get('Content-Type');
+    // Calculate response time
+    const endTime = new Date();
+    const responseTime = endTime - startTime;
+    const ipAddress = req.headers['cf-connecting-ip'];
+    const method = req.method;
+    const action = req.path;
+    const body = req.body;
+    const params = req.params;
+    const query = req.query;
+    const headers = req?.headers;
+    const file = req?.file;
+    const user = req?.user;
+    const date = new Date();
+    const fullPath = req?.originalUrl;
+    const level = "error";
+    const message = err.name;
+    const meta = {
+      error: err,
+      stack: err.stack,
+      ipAddress,
+      fullPath,
+      method,
+      action,
+      body,
+      params,
+      query,
+      headers,
+      file,
+      statusCode,
+      contentType,
+      response: storageData?.response ? JSON.parse(storageData?.response) : null,
+      user: user ? user._doc : null,
+      userId: user?._id ?? null,
+      date,
+      responseTime,
+    }
+    saveLogToMongoDB(level, message, meta)
+
+    next()
+  });
   let error = { ...err };
 
   error.message = err.message;
