ce52cbf6f42faa5fca7a02571232f8910e99deeb
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2023-12-09 04:04:59 +0300

    Update :general error messages

--- server/src/utils/error.js
+++ server/src/utils/error.js
@@ -7,6 +7,10 @@ class ErrorResponse extends Error {
   
   module.exports = {
     ErrorResponse,
+    NotFound: new ErrorResponse("Not found.", 404),
+    NotFoundUser: new ErrorResponse("Not found user.", 404),
     BadRequest: new ErrorResponse("Bad Request.", 400),
+    PasswordMisMatch: new ErrorResponse("Password does not match.", 400),
+    EmailExist: new ErrorResponse("Email already exists", 400),
   };
   
