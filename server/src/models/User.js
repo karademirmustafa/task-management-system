037433d5056a2b5d9960e5d29b3daa5b98d07412
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2023-12-09 04:39:21 +0300

    Bug fix : roles list imported

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const ROLES_LIST = require("../config/rolesList");
 
 const UserSchema = new mongoose.Schema(
   {
