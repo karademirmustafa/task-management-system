fdfc2e68d2d358fb1bbda6b9ce2c3ad389757d3c
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2023-12-09 19:57:07 +0300

    Add : password input

---
+++ client/src/components/shared/PasswordInput.js
@@ -0,0 +1,30 @@
+import { Input } from 'components/ui';
+import React, { useState } from 'react';
+import { HiOutlineEyeOff, HiOutlineEye } from 'react-icons/hi';
+
+const PasswordInput = (props) => {
+  const { onVisibleChange, ...rest } = props;
+
+  const [pwInputType, setPwInputType] = useState('password');
+
+  const onPasswordVisibleClick = (e) => {
+    e.preventDefault();
+    const nextValue = pwInputType === 'password' ? 'text' : 'password';
+    setPwInputType(nextValue);
+    onVisibleChange?.(nextValue === 'text');
+  };
+
+  return (
+    <Input
+      {...rest}
+      type={pwInputType}
+      suffix={
+        <span className="cursor-pointer text-xl text-end" onClick={(e) => onPasswordVisibleClick(e)}>
+          {pwInputType === 'password' ? <HiOutlineEyeOff /> : <HiOutlineEye />}
+        </span>
+      }
+    />
+  );
+};
+
+export default PasswordInput;
