f4426460e4e681c718af0589b86b5e4b218acbda
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2023-12-09 03:35:44 +0300

    server settings and stand-up process has been done

---
+++ server/src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const config = require("./config");
+const loaders = require("./loaders");
+
+const cors = require("cors");
+const helmet = require("helmet");
+
+const http = require("http");
+const server = http.createServer(app);
+
+// Load config
+config();
+
+//Load loaders
+loaders();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 6000;
+
+server.listen(PORT,() => {
+    console.log("Server is running!");
+})
