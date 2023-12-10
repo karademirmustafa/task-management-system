b1b39b5dd83997488e8591347418851931fbe0d1
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2023-12-10 12:57:32 +0300

    Add: task service

---
+++ client/src/services/TaskService.js
@@ -0,0 +1,38 @@
+import ApiService from './ApiService';
+
+export async function apiGetTasks(query) {
+  return ApiService.fetchData({
+    url: `/tasks${query}`,
+    method: 'get'
+  });
+}
+export async function apiGetTask(id) {
+  console.log(id,"id")
+  return ApiService.fetchData({
+    url: `/tasks/${id}`,
+    method: 'get'
+  });
+}
+export async function apiAddTask(data) {
+  return ApiService.fetchData({
+    url: `/tasks`,
+    method: 'post',
+    data
+  });
+}
+export async function apiEditTask(data,id) {
+  return ApiService.fetchData({
+    url: `/tasks/${id}`,
+    method: 'put',
+    data
+  });
+}
+export async function apiRemoveTask(data,id) {
+  return ApiService.fetchData({
+    url: `/tasks/${id}`,
+    method: 'delete',
+    data
+  });
+}
+
+
