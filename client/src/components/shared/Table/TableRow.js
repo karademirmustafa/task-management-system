24b5c81f61945ce35ed3274cf6eaa38edf869d3a
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2023-12-10 13:16:49 +0300

    Update: table column and row equals

--- client/src/components/shared/Table/TableRow.js
+++ client/src/components/shared/Table/TableRow.js
@@ -1,6 +1,14 @@
 
-const TableRow = ({ row }) => {
-  return <td className="px-6 py-4">{row}</td>;
+const TableRow = ({ row ,columns}) => {
+  return (
+    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+    {columns.map((column, index) => (
+      <td key={index} className="px-6 py-4">
+        {row[Object.keys(row).find(row => row===column.key)]}
+      </td>
+    ))}
+  </tr>
+  )
 };
 
 export default TableRow;
--- client/src/components/shared/Table/index.js
+++ client/src/components/shared/Table/index.js
@@ -1,27 +1,28 @@
-const { default: Column } = require('./Column');
-const { default: TableRow } = require('./TableRow');
+import Loading from "../Loading";
+import Column from "./Column";
+import TableRow from "./TableRow";
 
 const TableComponent = (props) => {
-  const { data=[], columns=[] } = props;
-  return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+  const { data = [], columns = [],loading } = props;
+  return (<>
+    {loading && <Loading/>}
+    {!loading && <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             {columns.map((column, index) => (
-              <Column key={index}>{column}</Column>
+              <Column key={index}>{column.cell}</Column>
             ))}
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            {data.map((row, i) => (
-              <TableRow key={i} row={row} />
-            ))}
-          </tr>
+          {data.map((row, i) => (
+            <TableRow key={i} row={row} columns={columns} />
+          ))}
         </tbody>
       </table>
-    </div>
+    </div>}
+    </>
   );
 };
 
