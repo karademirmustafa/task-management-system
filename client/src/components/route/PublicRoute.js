2ed00a143c9c0a27c3efb33a549549d69157be19
karademirmustafa <71528859+karademirmustafa@users.noreply.github.com>
2023-12-11 03:05:21 +0300

    Bug fix :

--- client/src/components/route/PublicRoute.js
+++ client/src/components/route/PublicRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import useAuth from 'utils/hooks/useAuth';
 import appConfig from 'configs/app.config';
 
@@ -7,8 +7,14 @@ const { authenticatedEntryPath } = appConfig;
 
 const PublicRoute = () => {
   const { authenticated } = useAuth();
+  const location = useLocation();
 
-  if (authenticated) {
+  const signInPath = '/sign-in';  
+  const signUpPath = '/sign-up';  
+
+  const isAuthRestrictedRoute = location.pathname === signInPath || location.pathname === signUpPath;
+
+  if (authenticated && isAuthRestrictedRoute) {
     return <Navigate to={authenticatedEntryPath} replace />;
   }
 
